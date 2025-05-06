1b19eb9e050a444752cb18493723f2452a10e507
ajacho <belen.jacho18@gmail.com>
2025-05-05 22:24:02 -0700

    Implement functions for question 2

--- client.py
+++ client.py
@@ -6,8 +6,12 @@ def get_programmer_count():
     Return the number of programmers return from the plural programmers API
     :return: An integer indicating the number of programmers in the plural list.
     """
-    return 0
-
+    url = "http://chrisbrooks.pythonanywhere.com/api/programmers"
+    response = requests.get(url)
+    data = response.json()
+    # Json data is a dictionary
+    return len(data["programmers"])
+    
 
 def get_programmer_by_id(pid):
     """
@@ -15,7 +19,19 @@ def get_programmer_by_id(pid):
     :param pid: Unique identifier for the programmer to lookup
     :return: A dictionary containing the matched programmer. Return an empty dictionary if not found
     """
-    return {}
+    url = f"http://chrisbrooks.pythonanywhere.com/api/programmers/{pid}"
+    response = requests.get(url)
+    programmerbyid = response.json()
+
+    if response.status_code == 404:
+        return {}
+
+    if not programmerbyid:
+        return {}
+
+    if not isinstance(programmerbyid, dict):
+        return {}
+    return programmerbyid
 
 
 def get_full_name_from_first(first_name):
@@ -24,4 +40,20 @@ def get_full_name_from_first(first_name):
     :param first_name:
     :return: A string containing the first and last name of the first programmer in the list of matches.
     """
-    return ""
+
+    url = "http://chrisbrooks.pythonanywhere.com/api/programmers/by_first_name/{first_name}"
+    url = url.replace("{first_name}", first_name)
+    data = requests.get(url)
+    data = data.json()
+    programmers = data["programmers"]
+    if not programmers:
+        return None
+    if isinstance(programmers, list):
+        for programmer in programmers:
+            if programmer["first"].lower() == first_name.lower():
+                return f"{programmer['first']} {programmer['last']}"
+    return None
+
+
+
+
